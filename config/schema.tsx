b98707df138c5a1ff88221a15c27de0aa8e9f8a3
NikolaSirakov2 <nikolasirakov1@gmail.com>
2025-07-27 15:06:46 +0300

    Add Drizzle ORM configuration with PostgreSQL support and update dependencies

---
+++ config/schema.tsx
@@ -0,0 +1,17 @@
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const productsTable = pgTable("products", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  price: integer().notNull(),
+  description: varchar({ length: 255 }).notNull(),
+  image: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp().notNull().defaultNow(),
+  updatedAt: timestamp().notNull().defaultNow(),
+});
